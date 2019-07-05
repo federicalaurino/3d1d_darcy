e3b7ffce99c058acf5db7eb4dc40c6998a81d878
federica <federica.laurino@polimi.it>
2019-07-05 19:08:24 +0200

    Work in progress HT..

---
+++ darcy3d1d_HT/include/darcy3d1d_precond.hpp
@@ -0,0 +1,78 @@
+/*  Class for darcy preconditioner for the 3D1D darcy problem:
+    P = [diag_t^-1 0  0 0; 0 schur_t^-1; 0 0 diag_v^-1 schur_v^-1]
+    where diag is the diagonal of the velocity mass matrix and 
+    schur is an approximation of the schur complement given by the jumps of the pressure
+    across the internal faces + a boundary term
+*/
+
+#include <darcy_precond.hpp>
+    
+template <typename Matrix> struct darcy3d1d_precond{
+    
+    //TODO add the possibility to have mixed bc !!!!!
+    
+    //gmm::diagonal_precond <Matrix> D;
+    //Matrix schur; 
+    
+    darcy_precond <Matrix> Prec_t;
+    darcy_precond <Matrix> Prec_v;
+    
+    getfem::size_type dof_ut;
+    getfem::size_type dof_pt;
+    getfem::size_type dof_uv;
+    getfem::size_type dof_pv;
+    
+    //getfem::size_type nrows = dof_u + dof_p;
+    //getfem::size_type ncols = dof_u + dof_p;
+    
+    
+    // default constructor
+    darcy3d1d_precond(void) {} 
+    //constructor
+    darcy3d1d_precond(const Matrix &A, const getfem::mesh_fem & mf_ut, const getfem::mesh_fem & mf_pt, const getfem::mesh_im & mimt,
+        const getfem::vector<getfem::mesh_fem> & mf_uvi, const getfem::mesh_fem & mf_pv, const getfem::mesh_im & mimv)
+        {
+        dof_ut = mf_ut.nb_dof();
+        dof_pt = mf_pt.nb_dof();
+        dof_uv = 0;
+        for (gmm::size_type i = 0; i<mf_uvi.size(); i++)
+            dof_uv += mf_uvi[i].nb_dof();
+        dof_pv = mf_pv.nb_dof();
+        
+        Matrix M;
+        gmm::copy(gmm::sub_matrix(A,
+                        gmm::sub_interval(0, dof_ut + dof_pt), gmm::sub_interval(0, dof_ut + dof_pt)), M);
+        // build the tissue precond 
+        Prec_t.build_with(M, mf_ut, mf_pt, mimt);
+        // build the vessel precond
+        gmm::clear(M);
+        gmm::copy(gmm::sub_matrix(A,
+                                         gmm::sub_interval(dof_ut + dof_pt, dof_uv + dof_pv), gmm::sub_interval(dof_ut + dof_pt, dof_uv + dof_pv)), M);
+        Prec_v.build_with(M, mf_uvi, mf_pv, mimv);        
+    } 
+    
+    }; // end of struct
+    
+    
+// Multiplication of the prec by a vector: P*vec -> res.
+// In our case we build P but what we have to do is P^-1*vec, therefore we solve the linear system P res =  vec 
+
+template < typename Matrix, typename V1, typename V2>
+void mult(const darcy3d1d_precond<Matrix> &P, const V1 &vec, V2 &res) {
+    
+    // multiplication of the tissue block
+    std::vector<gmm::size_type> ipvt(P.dof_ut + P.dof_pt);
+    for(gmm::size_type i=0; i< P.dof_ut + P.dof_pt; i++ ) ipvt[i] = i;
+    gmm::unsorted_sub_index interval;
+    interval = gmm::unsorted_sub_index(ipvt);
+    mult(P.Prec_t, gmm::sub_vector(vec, interval), gmm::sub_vector(res, interval));
+
+    // multiplication of the vessel block
+    std::vector<gmm::size_type> ipvt1(P.dof_uv + P.dof_pv);
+    for(gmm::size_type i=P.dof_ut + P.dof_pt; i< P.dof_ut + P.dof_pt + P.dof_uv + P.dof_pv; i++ ) ipvt1[i] = i;
+    gmm::unsorted_sub_index interval1;
+    interval1 = gmm::unsorted_sub_index(ipvt1);
+    mult(P.Prec_v, gmm::sub_vector(vec, interval1), gmm::sub_vector(res, interval1));
+    
+} // end mult
+
---
+++ darcy3d1d_HT/include/darcy_precond.hpp
@@ -0,0 +1,181 @@
+/*  Class for darcy preconditioner for the 3D darcy problem:
+    P = [diag^-1 0 ; 0 schur^-1]
+    where diag is the diagonal of the velocity mass matrix and 
+    schur is an approximation of the schur complement given by the jumps of the pressure
+    across the internal faces + a boundary term
+*/
+
+#include <gmm/gmm.h>
+#include <gmm/gmm_precond.h>
+//#include <getfem/getfem_mesh.h>
+#include <getfem/getfem_generic_assembly.h>
+#include <defines.hpp>
+
+    
+template <typename Matrix> struct darcy_precond{
+  
+    gmm::diagonal_precond <Matrix> D;
+    Matrix schur; 
+    
+    getfem::size_type dof_u;
+    getfem::size_type dof_p;
+    
+    getfem::size_type nrows = dof_u + dof_p;
+    getfem::size_type ncols = dof_u + dof_p;
+    
+    // Case of Dirichlet boundary condition
+    void build_with ( const Matrix &A, const getfem::mesh_fem & mf_u, const getfem::mesh_fem & mf_p, const getfem::mesh_im & mim){
+
+        dof_u = mf_u.nb_dof();
+        dof_p = mf_p.nb_dof();
+        
+        //build the preconditioner  blocks from the matrix A
+        
+        //diagonal block
+        
+        //Extract the mass matrix
+        Matrix M;
+        M.resize(dof_u, dof_u);
+        gmm::copy (gmm::sub_matrix(A,
+            gmm::sub_interval (0, dof_u),
+                                   gmm::sub_interval (0, dof_u)
+            ), M);
+        
+        D.build_with(M);
+        
+        //approximated schur block
+ 
+        gmm::resize(schur, dof_p, dof_p); gmm::clear(schur);
+        
+        const getfem::mesh &mesh = mf_p.linked_mesh();
+        getfem::mesh_region inner_faces = getfem::inner_faces_of_mesh(mesh);
+        getfem::mesh_region outer_faces;
+        getfem::outer_faces_of_mesh(mesh, outer_faces);
+        
+        getfem::ga_workspace wp;
+        std::vector<double> p(mf_p.nb_dof());
+        wp.add_fem_variable("p", mf_p, gmm::sub_interval(0,mf_p.nb_dof()), p);  
+
+        wp.add_expression( "1 / element_size * (p - Interpolate(p, neighbour_elt))"
+                            " * (Test_p - Interpolate(Test_p, neighbour_elt))",
+                            mim, inner_faces);
+        // to remove in case of mix/neumann condition
+        wp.add_expression("1/element_size*p*Test_p",
+                            mim, outer_faces);
+        //#endif
+        wp.assembly(2);
+
+        gmm::copy(wp.assembled_matrix(), schur);           
+        
+    } // end of build with
+    
+    // Case of Dirichlet boundary condition
+    void build_with ( const Matrix &A, const getfem::vector<getfem::mesh_fem> & mf_ui, const getfem::mesh_fem & mf_p, const getfem::mesh_im & mim){
+
+        dof_u = 0;
+        for( gmm::size_type i=0; i< mf_ui.size(); i++) 
+            dof_u += mf_ui[i].nb_dof();
+        dof_p = mf_p.nb_dof();
+        
+        //build the preconditioner  blocks from the matrix A
+        
+        //diagonal block
+        
+        //Extract the mass matrix
+        Matrix M;
+        M.resize(dof_u, dof_u);
+        gmm::copy (gmm::sub_matrix(A,
+            gmm::sub_interval (0, dof_u),
+                                   gmm::sub_interval (0, dof_u)
+            ), M);
+        
+        D.build_with(M);
+        
+        //approximated schur block
+ 
+        gmm::resize(schur, dof_p, dof_p); gmm::clear(schur);
+        
+        const getfem::mesh &mesh = mf_p.linked_mesh();
+        getfem::mesh_region inner_faces = getfem::inner_faces_of_mesh(mesh);
+        getfem::mesh_region outer_faces;
+        getfem::outer_faces_of_mesh(mesh, outer_faces);
+        
+        getfem::ga_workspace wp;
+        std::vector<double> p(mf_p.nb_dof());
+        wp.add_fem_variable("p", mf_p, gmm::sub_interval(0,mf_p.nb_dof()), p);  
+
+        wp.add_expression( "1 / element_size * (p - Interpolate(p, neighbour_elt))"
+                            " * (Test_p - Interpolate(Test_p, neighbour_elt))",
+                            mim, inner_faces);
+        // to remove in case of mix/neumann condition
+        wp.add_expression("1/element_size*p*Test_p",
+                            mim, outer_faces);
+        //#endif
+        wp.assembly(2);
+
+        gmm::copy(wp.assembled_matrix(), schur);           
+        
+    } // end of build with
+    
+    // default constructor
+    darcy_precond(void) {} 
+    //constructor
+    darcy_precond(const Matrix &A, const getfem::mesh_fem & mf_u, const getfem::mesh_fem & mf_p, const getfem::mesh_im & mim) {
+        build_with(A, mf_u, mf_p, mim); 
+    } 
+    //constructor
+    darcy_precond(const Matrix &A, const getfem::vector<getfem::mesh_fem> & mf_u, const getfem::mesh_fem & mf_p, const getfem::mesh_im & mim) {
+        build_with(A, mf_u, mf_p, mim); 
+    } 
+    
+    }; // end of struct
+    
+    
+// Multiplication of the prec by a vector: P*vec -> res.
+// In our case we build P but what we have to do is P^-1*vec, therefore we solve the linear system P res =  vec 
+
+template < typename Matrix, typename V1, typename V2>
+void mult(const darcy_precond<Matrix> &P, const V1 &vec, V2 &res) {
+    
+    // multiplication of the diag term -> use the mult method defined for diagonal_precond
+    gmm::mult(P.D,
+              gmm::sub_vector(vec, gmm::sub_interval(0, P.dof_u)), 
+              gmm::sub_vector(res, gmm::sub_interval(0, P.dof_u)));
+    
+    // multiplication of the Schur term
+    double cond;
+    gmm::SuperLU_solve(P.schur, 
+                       gmm::sub_vector(res, gmm::sub_interval(P.dof_u, P.dof_p)), 
+                       gmm::sub_vector(vec, gmm::sub_interval(P.dof_u, P.dof_p)), 
+                       cond);
+} // end mult
+
+//Redefinition of some gmm functions for darcy_precond
+
+namespace gmm{
+    
+struct owned_implementation {};
+
+template <>
+    struct principal_orientation_type<owned_implementation> {
+        using potype = owned_implementation;
+    }; 
+
+template <typename MATRIX>
+ struct linalg_traits<::darcy_precond<MATRIX>> {
+        using this_type = ::darcy_precond<MATRIX>;
+        using sub_orientation = owned_implementation;
+
+        static gmm::size_type nrows(const this_type &m) { return m.nrows; }
+        static gmm::size_type ncols(const this_type &m) { return m.ncols; }
+    };   
+    
+template <typename L1, typename L2, typename L3>
+    inline
+    void mult_spec(const L1 &m, const L2 &src, L3 &dst, owned_implementation tag)
+    {
+       mult(m, src, dst);
+    }
+
+}    
+ // end of namespace gmm
